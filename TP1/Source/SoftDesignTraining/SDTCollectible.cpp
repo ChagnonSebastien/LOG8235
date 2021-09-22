b023af1c4b67f601b9562c16c9ae2e8b4abdce31
jason thai <jason.thai@polymtl.ca>
2021-09-22 14:52:07 -0400

    Complété Exercice 5: Déplacement d'un collectible

--- TP1/Source/SoftDesignTraining/SDTCollectible.cpp
+++ TP1/Source/SoftDesignTraining/SDTCollectible.cpp
@@ -2,6 +2,7 @@
 
 #include "SDTCollectible.h"
 #include "SoftDesignTraining.h"
+#include "SDTUtils.h"
 
 ASDTCollectible::ASDTCollectible()
 {
@@ -11,6 +12,8 @@ ASDTCollectible::ASDTCollectible()
 void ASDTCollectible::BeginPlay()
 {
     Super::BeginPlay();
+    m_initialPosition = GetActorLocation();
+    SetToInitialState();
 }
 
 void ASDTCollectible::Collect()
@@ -25,6 +28,7 @@ void ASDTCollectible::OnCooldownDone()
     GetWorld()->GetTimerManager().ClearTimer(m_CollectCooldownTimer);
 
     GetStaticMeshComponent()->SetVisibility(true);
+    SetToInitialState();
 }
 
 bool ASDTCollectible::IsOnCooldown()
@@ -32,7 +36,34 @@ bool ASDTCollectible::IsOnCooldown()
     return m_CollectCooldownTimer.IsValid();
 }
 
+void ASDTCollectible::SetToInitialState()
+{
+    SetActorLocation(m_initialPosition);
+    m_speed = FVector(0.f, MAX_SPEED, 0.f);
+    m_acceleration = FVector(0.f, MAX_ACCELERATION, 0.f);
+}
+
 void ASDTCollectible::Tick(float deltaTime)
 {
     Super::Tick(deltaTime);
+
+    if (isMoveable && !IsOnCooldown()) {
+
+        UWorld* world = GetWorld();
+        FVector const objectPos = GetActorLocation();
+
+        // deltaT pour atteindre une vitesse nulle
+        float deltaT = abs(m_speed.Size() / m_acceleration.Size());
+        // xf - xi = v*t + 1/2 * a * t^2 sachant que xi = 0
+        float dist = m_speed.Size() * deltaT - 1 / 2 * m_acceleration.Size() * deltaT * deltaT;
+
+        if (SDTUtils::Raycast(world, FVector(FVector(objectPos)), FVector(FVector(objectPos)) + dist * m_acceleration.GetSafeNormal())) {
+            m_acceleration *= -1.f;
+        }
+
+        FVector const displacement = m_speed * deltaTime;
+        SetActorLocation(objectPos + displacement, true);
+        FVector temp_speed = m_speed + m_acceleration * deltaTime;
+        m_speed = (MAX_SPEED > abs(temp_speed.Y)) ? temp_speed : (temp_speed.GetSafeNormal() * MAX_SPEED);
+    }
 }
--- TP1/Source/SoftDesignTraining/SDTCollectible.h
+++ TP1/Source/SoftDesignTraining/SDTCollectible.h
@@ -7,21 +7,21 @@
 #include "SDTCollectible.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SOFTDESIGNTRAINING_API ASDTCollectible : public AStaticMeshActor
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 public:
     ASDTCollectible();
 
     void Collect();
     void OnCooldownDone();
-    bool IsOnCooldown(); 
+    bool IsOnCooldown();
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
-    float m_CollectCooldownDuration = 10.f;
+        float m_CollectCooldownDuration = 10.f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
         bool isMoveable = false;
@@ -29,9 +29,17 @@ public:
     virtual void Tick(float deltaTime) override;
     virtual void BeginPlay() override;
 
-    FVector initialPosition;
+    float const MAX_SPEED = 500.0f;
+    float const MAX_ACCELERATION = 500.0f;
+
+    FVector m_initialPosition;
+    // float m_speed;
+    FVector m_speed;
+    FVector m_acceleration;
+    // bool m_isGoingRight = true;
 
 protected:
+    void SetToInitialState();
+
     FTimerHandle m_CollectCooldownTimer;
-	
 };
