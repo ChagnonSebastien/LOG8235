6cc2664792326e91a647126243c21767d7df0fa9
Sebastien Chagnon <sebastien.chagnon@e-mhicc.org>
2021-09-30 15:21:51 -0400

    Added our own new custom GameState

--- TP1/Source/SoftDesignTraining/SoftDesignTrainingGameMode.cpp
+++ TP1/Source/SoftDesignTraining/SoftDesignTrainingGameMode.cpp
@@ -4,11 +4,15 @@
 #include "SoftDesignTraining.h"
 #include "SoftDesignTrainingPlayerController.h"
 #include "SoftDesignTrainingCharacter.h"
+#include "SoftDesignTrainingGameState.h"
 
 ASoftDesignTrainingGameMode::ASoftDesignTrainingGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = ASoftDesignTrainingPlayerController::StaticClass();
+
+	// use our custom GameState class
+	GameStateClass = ASoftDesignTrainingGameState::StaticClass();
 
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/BP_SDTMainCharacter"));
---
+++ TP1/Source/SoftDesignTraining/SoftDesignTrainingGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "SoftDesignTrainingGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOFTDESIGNTRAINING_API ASoftDesignTrainingGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
