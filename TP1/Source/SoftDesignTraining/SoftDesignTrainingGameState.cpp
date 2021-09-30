39f83e366d1a95fc37a57d39ecb8da1681caf5b3
AlexandreRamt <alexandre.ramt@gmail.com>
2021-09-30 16:16:18 -0400

    Fixed GameState cpp file

--- TP1/Source/SoftDesignTraining/SoftDesignTrainingGameState.cpp
+++ TP1/Source/SoftDesignTraining/SoftDesignTrainingGameState.cpp
@@ -3,3 +3,19 @@
 
 #include "SoftDesignTrainingGameState.h"
 
+void ASoftDesignTrainingGameState::incrementDeaths() {
+	this->numberOfDeaths += 1;
+}
+
+void ASoftDesignTrainingGameState::incrementCollected() {
+	this->numberOfCollected += 1;
+}
+
+
+int ASoftDesignTrainingGameState::getDeaths() {
+	return this->numberOfDeaths;
+}
+
+int ASoftDesignTrainingGameState::getCollected() {
+	return this->numberOfCollected;
+}
--- TP1/Source/SoftDesignTraining/SoftDesignTrainingGameState.h
+++ TP1/Source/SoftDesignTraining/SoftDesignTrainingGameState.h
@@ -13,5 +13,17 @@ UCLASS()
 class SOFTDESIGNTRAINING_API ASoftDesignTrainingGameState : public AGameState
 {
 	GENERATED_BODY()
-	
+public:
+
+
+	void incrementDeaths();
+	void incrementCollected();
+
+	int getDeaths();
+	int getCollected();
+
+private:
+	int numberOfDeaths = 0;
+	int numberOfCollected = 0;
 };
+
