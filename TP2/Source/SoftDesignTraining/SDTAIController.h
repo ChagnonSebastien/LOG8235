a9c180a0ec08584eb000a1d1ccd90d01ef3e377d
ph.trempe@gmail.com <ph.trempe@gmail.com>
2021-11-07 11:00:00 -0500

    Added documentation for SDTAIController.h

--- TP2/Source/SoftDesignTraining/SDTAIController.h
+++ TP2/Source/SoftDesignTraining/SDTAIController.h
@@ -13,7 +13,7 @@
 class USDTPathFollowingComponent;
 
 /**
- * 
+ * An AI controller that is used to manage the agent's state, behavior and interactions with the player.
  */
 UCLASS(ClassGroup = AI, config = Game)
 class SOFTDESIGNTRAINING_API ASDTAIController : public ASDTBaseAIController
@@ -23,42 +23,55 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public ASDTBaseAIController
 public:
     ASDTAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
+    /** The speed at which the agent moves (in cm/s). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_MovementSpeed = 250.f;
 
+    /** The candidate next speed (in cm/s). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_NewSpeed = 250.f;
 
+    /** The speed at which the agent walks (in cm/s). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_WalkingSpeed = 250.f;
 
+    /** The speed at which the agent runs (in cm/s). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_RunningSpeed = 600.f;
 
+    /** The half-length of the detection capsule (in cm). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_DetectionCapsuleHalfLength = 500.f;
 
+    /** The radius of the detection capsule (in cm). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_DetectionCapsuleRadius = 250.f;
 
+    /** The forward starting offset of the detection capsule (in cm). */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_DetectionCapsuleForwardStartingOffset = 100.f;
 
+    /** The agent's jump curve that describes its movement over death traps. */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     UCurveFloat* JumpCurve;
 
+    /** The height of the highest point in the agent's jump curve. */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float JumpApexHeight = 300.f;
 
+    /** The agent's speed when jumping. */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float JumpSpeed = 1.f;
 
+    /** If the agent is at a jump segment. */
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = AI)
     bool AtJumpSegment = false;
 
+    /** If the agent is in the air (jumping). */
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = AI)
     bool InAir = false;
 
+    /** If the agent is landing. */
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = AI)
     bool Landing = true;
 
